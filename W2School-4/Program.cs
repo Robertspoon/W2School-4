29633911ab36a45e120d98a38d52b3f7d19b76b3
Robert Atkinson <robertthemcjug@gmail.com>
2022-10-09 19:42:18 -0300

    Initial commit

---
+++ W2School-4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W2School_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int i = 0;
+            // do
+            // {
+            //     Console.WriteLine(i);
+            //     i++;
+            // }
+            // while (i < 10);
+
+           for (int i = 0; i < 9; i++)
+           {
+               if (i ==4)
+               {
+                    //break;
+                    continue;
+               }
+                Console.WriteLine(i);
+           }
+
+
+            // for (int i = 0; i <= 720; i = i + 20)
+            //{
+            //      Console.WriteLine(i);
+
+            //string[] peopleIDontReallyLike = { "You", "Myself", "Simon Cowell" };
+           // foreach (string i in peopleIDontReallyLike)
+               // Console.WriteLine("People I dont really like: " + i);
+
+          Console.ReadKey(true);  
+
+               
+        }
+    }
+}
